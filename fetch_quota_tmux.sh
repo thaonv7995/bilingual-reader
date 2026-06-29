39edbd86197530fbc48bf300c0255846521ce991
thaonv <thaonv@thaonv.site>
2026-06-29 12:47:36 +0700

    style: sync root fetch_quota_tmux.sh with books_cli implementation

--- fetch_quota_tmux.sh
+++ fetch_quota_tmux.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
-SESSION_NAME="agy_quota_fetch"
+SESSION_NAME="agy_quota_fetch_$$"
 
 AGY_BIN="${1:-agy}"
 
-# Kill any existing session with this name
+# Kill any existing session with this name just in case
 tmux kill-session -t $SESSION_NAME 2>/dev/null
 
-# Start a new detached tmux session running agy
+# Start a new detached tmux session running agy in current directory
 tmux new-session -d -s $SESSION_NAME -x 1000 -y 100 "$AGY_BIN"
 
-# Wait a moment for agy to load and show the prompt
-sleep 4
+# Wait a moment for agy to load
+sleep 3
 
 # Send the "/quota" command and press Enter
 tmux send-keys -t $SESSION_NAME "/quota" C-m
 
 # Wait for the quota to be printed
-sleep 4
+sleep 3
 
 # Capture the current screen output
-tmux capture-pane -t $SESSION_NAME -p -S - -E -
+tmux capture-pane -t $SESSION_NAME -p -S - -E - | sed '/^[[:space:]]*$/d'
 
 # Kill the session
 tmux kill-session -t $SESSION_NAME
